9f1d96095c31dea15b5c154ff1185676abfad45c
Fanny Simon <contact.fanny.simon@gmail.com>
2025-07-07 11:37:47 +0200

    Affichage des projets dynamiquement dans chaque <figure>, dans la div .gallery

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -2,16 +2,40 @@ let projects = [];
 
 // Fonction pour aller chercher les données de l'API
 async function fetchProjects() {
-  await fetch('http://localhost:5678/api/works')
+  await fetch("http://localhost:5678/api/works")
     .then((res) => res.json()) // On transforme la réponse en JSON
     .then((data) => {
       projects = data; // Stocke les données API dans "projects"
       console.log(projects); // toujours ce garder l'objet ouvert dans la console
-      // projectsDisplay(); // ⬅️ On appelle cette fonction après avoir reçu les données
+      displayProjects(); //  On appelle cette fonction après avoir reçu les données
     })
     .catch((error) =>
       console.error("Erreur lors de la récupération des projets :", error)
     ); // Gestion des erreurs
+}
+
+// Fonction pour afficher les projets :
+// on créer une figure pour chaque projet
+// on y ajoute l'image et le titre
+// on ajoute la figure dans la gallery.
+
+function displayProjects() {
+  projects.forEach((project) => {
+    const gallery = document.querySelector(".gallery");
+    const figure = document.createElement("figure");
+
+    const img = document.createElement("img");
+    img.src = project.imageUrl;
+    img.alt = project.title;
+
+    const caption = document.createElement("figcaption");
+    caption.innerText = project.title;
+
+    figure.appendChild(img);
+    figure.appendChild(caption);
+
+    gallery.appendChild(figure);
+  });
 }
 
 // Charger les projets au démarrage
